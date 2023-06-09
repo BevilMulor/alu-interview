39b8e4ffc98cdda07a5e07b95932b5fee0c23301
BevilMulor <109423584+BevilMulor@users.noreply.github.com>
2023-06-09 05:53:55 +0000

    rain

--- rain/0-rain.py
+++ rain/0-rain.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
-"""#Amount of water retained after it rains in square units"""
+"""Calculate how many square units of water will be retained after it rains."""
 
 
 def rain(walls):
     """
-    #Amount of water retained after it rains.
+    Calculate the amount of water retained after it rains.
 
     Args:
         walls (list): non-negative integers .
@@ -36,42 +36,4 @@ def rain(walls):
     for i in range(n):
         water_units += min(left_max[i], right_max[i]) - walls[i]
 
-    return #!/usr/bin/python3
-"""#Amount of water retained after it rains in square units"""
-
-
-def rain(walls):
-    """
-    #Amount of water retained after it rains.
-
-    Args:
-        walls (list): non-negative integers .
-
-    Returns:
-        int: The total number of square units of water retained.
-
-    """
-
-    if not walls:
-        return 0
-
-    n = len(walls)
-    left_max = [0] * n
-    right_max = [0] * n
-
-    # Calculate the maximum height to the left of each wall
-    left_max[0] = walls[0]
-    for i in range(1, n):
-        left_max[i] = max(left_max[i-1], walls[i])
-
-    # Calculate the maximum height to the right of each wall
-    right_max[n-1] = walls[n-1]
-    for i in range(n-2, -1, -1):
-        right_max[i] = max(right_max[i+1], walls[i])
-
-    rain_water = 0
-    # Calculate the amount of water retained above each wall
-    for i in range(n):
-        rain_water += min(left_max[i], right_max[i]) - walls[i]
-        
-    return rain_water
+    return water_units
